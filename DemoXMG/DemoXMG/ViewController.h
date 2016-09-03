fd92da71145c831535e493f6e89fcaf6f6871dd8
xiaoxi <xiaoxi@126.com>
2016-09-03 09:00:22 +0800

    初始化项目

---
+++ DemoXMG/DemoXMG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoXMG
+//
+//  Created by iso9007 on 16/9/3.
+//  Copyright © 2016年 iso9007. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoXMG/DemoXMG/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DemoXMG
+//
+//  Created by iso9007 on 16/9/3.
+//  Copyright © 2016年 iso9007. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
